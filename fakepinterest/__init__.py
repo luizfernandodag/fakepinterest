35b6a3defa0b5718a883aeb0efcaa6140a63b67c
luizfernandodag <luizfernandodag@gmail.com>
2024-02-25 17:37:59 -0300

    updated flask version

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/local'  # Update with your MySQL database credentials
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://banco_fakepinterest_xrsn_user:c2feKZOZKjSmuvNDQLzutfSFKv8qarv8@dpg-cnd8v86g1b2c739litp0-a.oregon-postgres.render.com/banco_fakepinterest_xrsn"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://banco_fakepinterest_xrsn_user:c2feKZOZKjSmuvNDQLzutfSFKv8qarv8@dpg-cnd8v86g1b2c739litp0-a.oregon-postgres.render.com/banco_fakepinterest_xrsn"
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/databasename'  # Update with your MySQL database credentials
 
 app.config["SECRET_KEY"] = "59d3e5bf433a5789c3dca5ff53c15983"
